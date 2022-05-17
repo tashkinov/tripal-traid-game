52f8f2511f8fba97591f0794b11978755cadd29f
tashkinov <3dmeloman@gmail.com>
2022-05-17 15:27:00 +0300

    lesson-06

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,21 @@
-import {Outlet, useMatch} from 'react-router-dom';
+import {Outlet, useMatch, useLocation} from 'react-router-dom';
 import Container from '../Container';
 
 import Header from '../Header';
 import Footer from "../Footer";
 
 import style from './Layout.module.scss';
+import {useEffect} from "react";
 
 const Layout = () => {
+
     const match = useMatch({path: '/'});
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname])
+
     return (
         <>
             <Header />
